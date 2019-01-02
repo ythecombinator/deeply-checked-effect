fb853af1763c92e1cc93300cea6d73d467c927f0
Matheus Albuquerque <matheus.brasil10@gmail.com>
2019-01-02 11:04:46 -0300

    docs(example): add basic usage sample

---
+++ example.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import useDeeplyCheckedEffect from "./";
+
+function UseEffect({ state }) {
+  useEffect(
+    () => {
+      console.log("useEffect", state);
+    },
+    [state]
+  );
+
+  return <div />;
+}
+
+function UseDeeplyCheckedEffect({ state }) {
+  useDeeplyCheckedEffect(
+    () => {
+      console.log("useDeeplyCheckedEffect", state);
+    },
+    [state]
+  );
+
+  return <div />;
+}
+
+function App() {
+  const [state, setState] = useState({ count: 1 });
+  const [state2, setState2] = useState({ count: 1 });
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setState({ ...state, count: state.count });
+        }}
+      >
+        Logs `useEffect`
+      </button>
+      <button
+        onClick={() => {
+          setState({ ...state, count: state.count + 1 });
+        }}
+      >
+        Logs both
+      </button>
+      <UseEffect state={state} />
+      <UseDeeplyCheckedEffect state={state} />
+    </div>
+  );
+}
+
+render(<App />, window.root);
